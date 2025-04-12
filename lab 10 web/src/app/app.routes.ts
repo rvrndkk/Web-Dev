e7d3c4af5baea311f4eb1a47a419a7c6adbf39d5
rvrndkk <k_tregub@kbtu.kz>
2025-04-12 16:44:25 +0600

    Delete lab 10 web directory

--- lab 10 web/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CompanyListComponent } from './components/company-list/company-list.component';
-import { CompanyDetailComponent } from './components/company-detail/company-detail.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'companies', pathMatch: 'full' },
-  { path: 'companies', component: CompanyListComponent },
-  { path: 'companies/:id', component: CompanyDetailComponent }
-];
